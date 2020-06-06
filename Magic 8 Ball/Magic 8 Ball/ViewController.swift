36cea51282250d196268900e39abc2366e833b2d
jervygu <jervygu@gmail.com>
2020-06-06 09:48:22 +0800

    Created Xcode Project

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Jaypee Umandap on 6/6/20.
+//  Copyright © 2020 Jervy Umandap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
